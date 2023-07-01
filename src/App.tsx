defbf315136bdb6c4125c93477341131701c59ce
Adam Hudson <adamnubbz@gmail.com>
2023-07-01 07:52:33 -0500

    navbar pieces

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,32 @@ import Navbar from "./sections/Navbar";
 import Wrapper from "./sections/Wrapper";
 import Footer from "./sections/Footer";
 import Background from "./components/Background";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import "./scss/index.scss";
+import Search from "./pages/Search";
+import MyList from "./pages/MyList";
+import About from "./pages/About";
+import Compare from "./pages/Compare";
+import Pokemon from "./pages/Pokemon";
 
 function App() {
   return (
     <div className="main-container">
       <Background />
-      <div className="app">
-        <Navbar></Navbar>
-        <Wrapper></Wrapper>
-        <Footer></Footer>
-      </div>
+      <BrowserRouter>
+        <div className="app">
+          <Navbar></Navbar>
+          <Routes>
+            <Route element={<Search />} path="/search" />
+            <Route element={<MyList />} path="/list" />
+            <Route element={<About />} path="/about" />
+            <Route element={<Compare />} path="/compare" />
+            <Route element={<Pokemon />} path="/pokemon/:id" />
+            <Route element={<Navigate to="/pokemon/1" />} path="*" />
+          </Routes>
+          <Footer></Footer>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
