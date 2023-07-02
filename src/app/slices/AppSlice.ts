38370cd1d4f17708db633a771db2ea6916d6e88a
Adam Hudson <adamnubbz@gmail.com>
2023-07-02 16:02:48 -0500

    finish grid, setting up compare slice and reducer

--- src/app/slices/AppSlice.ts
+++ src/app/slices/AppSlice.ts
@@ -1,12 +1,20 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { AppTypeInitialState } from "../../utils/Types";
 
-const initialState: AppTypeInitialState = {};
+const initialState: AppTypeInitialState = {
+  toasts: [],
+};
 
 export const AppSlice = createSlice({
   name: "app",
   initialState,
-  reducers: {},
+  reducers: {
+    setToast: (state, action) => {
+      const toasts = [...state.toasts];
+      toasts.push(action.payload);
+      state.toasts = toasts;
+    },
+  },
 });
 
 export const {} = AppSlice.actions;
--- src/components/PokemonCardGrid.tsx
+++ src/components/PokemonCardGrid.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { IoGitCompare } from "react-icons/io5";
+import { FaPlus, FaTrash } from "react-icons/fa";
+import { useLocation, useNavigate } from "react-router-dom";
 import { pokemonTypeInterface, userPokemonsType } from "../utils/Types";
+import { useAppDispatch } from "../app/hooks";
+import { addToCompare } from "../app/slices/PokemonSlice";
 
 function PokemonCardGrid({ pokemons }: { pokemons: userPokemonsType[] }) {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+
   return (
     <div className="pokemon-card-grid-container">
       <div className="pokemon-card-grid">
@@ -10,14 +19,24 @@ function PokemonCardGrid({ pokemons }: { pokemons: userPokemonsType[] }) {
           pokemons?.map((data: userPokemonsType) => {
             return (
               <div className="pokemon-card" key={data.id}>
-                <div className="pokemon-card-list"></div>
-                <div className="pokemon-card-compare"></div>
+                <div className="pokemon-card-list">
+                  {location.pathname.includes("/pokemon") ||
+                  location.pathname.includes("/search") ? (
+                    <FaPlus className="plus" />
+                  ) : (
+                    <FaTrash className="trash" />
+                  )}
+                </div>
+                <div className="pokemon-card-compare">
+                  <IoGitCompare onClick={() => dispatch(addToCompare(data))} />
+                </div>
                 <h3 className="pokemon-card-title">{data.name}</h3>
                 <img
                   src={data.image}
                   alt="pokemon image"
                   className="pokemon-card-image"
                   loading="lazy"
+                  onClick={() => navigate(`/pokemon/${data.id}`)}
                 />
                 <div className="pokemon-card-types">
                   {data.types.map(
