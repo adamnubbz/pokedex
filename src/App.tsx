5a33deeda9c7f3d223fa1576a3462fff171f86d7
Adam Hudson <adamnubbz@gmail.com>
2023-07-04 18:38:30 -0500

    so close, pokemon error in info yet to resolve

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect } from "react";
 import Navbar from "./sections/Navbar";
-import Wrapper from "./sections/Wrapper";
 import Footer from "./sections/Footer";
 import Background from "./components/Background";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
